4e8e1c3f454a8945631fcc2f5b974b6dd3614190
Maxence <144704028+WolverineFR@users.noreply.github.com>
2025-05-12 16:39:36 +0200

    Mise à jour de la bdd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,15 @@
-INSERT INTO `user` (username, email, password) VALUES 
-('Jean964', 'jeanjean@email.com', 'mouton56'),
-('Pierre42', 'pierre-berger@email.com', 'jaimelesfleurs');
+INSERT INTO dbuser (username, email, password, balance) VALUES 
+('Jean964', 'jeanjean@email.com', '$2a$10$Zd6iahffw6N11rTySBO4bOu35lLvdRpbg968nex8L5CvNSuUg44Pm', 100.00),
+('Pierre42', 'pierre-berger@email.com', '$2a$10$9mptt2ZUeBUtrE38mmCieOZX6zyQsUdOE.Gy8IuC.rHR3UP/xAcOm', 20.00);
 
-INSERT INTO transaction (sender_id, receiver_id, description, amount) VALUES 
-(1, 2, 'Cadeau de noel', 120.50);
-
-INSERT INTO user_connection (user_id, connection_id) VALUES 
+INSERT INTO user_friend (user_id, friend_id) VALUES 
 (1, 2), (2, 1);
+
+INSERT INTO dbtransaction (sender_id, receiver_id, description, amount, fee) VALUES 
+(1, 2, 'Cadeau de noel', 20.00, 0.10);
+
+INSERT INTO app_wallet (transaction_id, fee_amount)
+VALUES (1, 0.10);
+
+UPDATE dbuser SET balance = balance - 20.00 WHERE id = 1;
+UPDATE dbuser SET balance = balance + 19.90 WHERE id = 2;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,29 +1,40 @@
-DROP TABLE IF EXISTS user_connection;
-DROP TABLE IF EXISTS transaction;
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS user_friend;
+DROP TABLE IF EXISTS dbtransaction;
+DROP TABLE IF EXISTS dbuser;
+DROP TABLE IF EXISTS app_wallet;
 
-CREATE TABLE `user` (
+CREATE TABLE dbuser (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    balance DECIMAL(10,2) DEFAULT 0
 );
 
-CREATE TABLE transaction (
+CREATE TABLE dbtransaction (
     id INT AUTO_INCREMENT PRIMARY KEY,
     sender_id INT NOT NULL,
     receiver_id INT NOT NULL,
     description VARCHAR(255),
     amount DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (sender_id) REFERENCES user(id),
-    FOREIGN KEY (receiver_id) REFERENCES user(id)
+    fee DECIMAL(10,2) NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES dbuser(id),
+    FOREIGN KEY (receiver_id) REFERENCES dbuser(id)
 );
 
-CREATE TABLE user_connection (
+CREATE TABLE user_friend (
     user_id INT,
-    connection_id INT,
-    PRIMARY KEY (user_id, connection_id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (connection_id) REFERENCES user(id)
+    friend_id INT,
+    PRIMARY KEY (user_id, friend_id),
+    FOREIGN KEY (user_id) REFERENCES dbuser(id),
+    FOREIGN KEY (friend_id) REFERENCES dbuser(id)
 );
 
+CREATE TABLE app_wallet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    transaction_id INT NOT NULL,
+    fee_amount DECIMAL(10,2) NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (transaction_id) REFERENCES dbtransaction(id)
+);
