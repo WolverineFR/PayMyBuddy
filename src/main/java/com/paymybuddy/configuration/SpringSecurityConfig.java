2f6fd9ae3c6b8bff3396230185be72d2148651a6
Maxence <144704028+WolverineFR@users.noreply.github.com>
2025-05-20 16:35:40 +0200

    Modification et suppresion du JWT et ajout des pages html

--- src/main/java/com/paymybuddy/configuration/SpringSecurityConfig.java
+++ src/main/java/com/paymybuddy/configuration/SpringSecurityConfig.java
@@ -1,28 +1,15 @@
 package com.paymybuddy.configuration;
 
-import javax.crypto.spec.SecretKeySpec;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-
-import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.paymybuddy.service.CustomUserDetailsService;
 
 @Configuration
@@ -32,22 +19,41 @@ public class SpringSecurityConfig {
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
 
-	@Value("${jwt.secret}")
-	private String jwtKey;
-
+	/*
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/login", "/register").permitAll().requestMatchers("/admin")
-								.hasRole("ADMIN").requestMatchers("/user").hasRole("USER").anyRequest().authenticated())
-
+						auth -> auth.requestMatchers("/","/index.html","/css/**","/login","/login.html","/register", "/register").permitAll()
+						.requestMatchers("/admin").hasRole("ADMIN")
+						.requestMatchers("/user").hasRole("USER")
+						.anyRequest().authenticated())
 				.oauth2ResourceServer(
 						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
-
 				.build();
 	}
+	*/
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    return http
+	        .csrf(csrf -> csrf.disable())
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/", "/index.html","/login", "/register", "/css/**").permitAll()
+	            .anyRequest().authenticated()
+	        )
+	        .formLogin(form -> form
+	            .defaultSuccessUrl("/user", true)
+	            .permitAll()
+	        )
+	        .logout(logout -> logout
+	                .logoutUrl("/logout")
+	                .logoutSuccessUrl("/login?logout")
+	            )
+	        .build();
+	}
+/*
 
 	@Bean
 	public JwtDecoder jwtDecoder() {
@@ -70,12 +76,9 @@ public class SpringSecurityConfig {
 		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
 		return jwtAuthenticationConverter;
 	}
-
+*/
 	@Bean
 	public UserDetailsService users() {
-		// UserDetails user =
-		// User.builder().username("user").password(passwordEncoder().encode("user")).roles("USER").build();
-
 		return customUserDetailsService;
 	}
 
--- src/main/java/com/paymybuddy/controller/LoginController.java
+++ src/main/java/com/paymybuddy/controller/LoginController.java
@@ -1,46 +1,41 @@
 package com.paymybuddy.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.paymybuddy.model.DBUser;
 import com.paymybuddy.repository.UserRepository;
-import com.paymybuddy.service.JWTService;
-import com.paymybuddy.service.UserService;
 
-@RestController
+import org.springframework.ui.Model;
+
+
+@Controller
 public class LoginController {
-
-	public JWTService jwtService;
 
 	@Autowired
 	private UserRepository userRepository;
-
-	@Autowired
-	private UserService userSerice;
-
-	private final AuthenticationManager authenticationManager;
-
-	public LoginController(JWTService jwtService, AuthenticationManager authenticationManager) {
-		this.authenticationManager = authenticationManager;
-		this.jwtService = jwtService;
+	
+	/*
+	@GetMapping("/login")
+	public String showLoginPage() {
+		return "login";
 	}
 
-	@PostMapping("/token")
-	public String getToken(Authentication auth) {
-		String token = jwtService.generateToken(auth);
-		return token;
+	*/
+	
+	@GetMapping("/user")
+	public String getUserPage(Authentication authentication, Model model) {
+	    String username = authentication.getName();
+	    DBUser user = userRepository.findByUsername(username);
+	    model.addAttribute("user", user);
+	    return "user";
 	}
 
+	
 	/*
 	 * private OAuth2AuthorizedClientService authorizedClientService;
 	 * 
@@ -48,7 +43,7 @@ public class LoginController {
 	 * authorizedClientService) { this.authorizedClientService =
 	 * authorizedClientService; }
 	 */
-
+/*
 	@PostMapping("/login")
 	public ResponseEntity<?> loginUser(@RequestBody DBUser user) {
 		try {
@@ -60,7 +55,7 @@ public class LoginController {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Identifiants invalides");
 		}
 	}
-
+*/
 	/*
 	 * 
 	 * @PostMapping("/register") public ResponseEntity<?> registerUser(@RequestBody
@@ -74,18 +69,6 @@ public class LoginController {
 	 * 
 	 */
 
-	@GetMapping("/")
-	public String getResource() {
-		return "a value...";
-	}
 
-	@GetMapping("/user")
-	public String getUser() {
-		return "Bienvenue, user";
-	}
 
-	@GetMapping("/admin")
-	public String getAdmin() {
-		return "Bienvenue, Admin";
-	}
 }
